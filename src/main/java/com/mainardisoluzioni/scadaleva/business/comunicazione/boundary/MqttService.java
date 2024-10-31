4c86d5bfa50749d59b34eba09070a2e9dde5669a
adminavvimpa <adminavvimpa@nb-telelavoro-3>
2024-10-31 16:15:16 +0100

    Ora è possibile leggere i messaggi MQTT senza bloccare l'applicazione.

--- src/main/java/com/mainardisoluzioni/scadaleva/business/comunicazione/boundary/MqttService.java
+++ src/main/java/com/mainardisoluzioni/scadaleva/business/comunicazione/boundary/MqttService.java
@@ -4,25 +4,15 @@
  */
 package com.mainardisoluzioni.scadaleva.business.comunicazione.boundary;
 
-import static com.hivemq.client.internal.mqtt.util.MqttChecks.topic;
-import com.hivemq.client.mqtt.MqttGlobalPublishFilter;
-import com.hivemq.client.mqtt.datatypes.MqttQos;
-import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
-import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient.Mqtt5Publishes;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
-import com.hivemq.client.mqtt.mqtt5.message.connect.connack.Mqtt5ConnAck;
-import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
+import com.hivemq.client.mqtt.mqtt5.message.subscribe.Mqtt5RetainHandling;
 import com.hivemq.client.mqtt.mqtt5.message.subscribe.suback.Mqtt5SubAck;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import jakarta.ejb.Asynchronous;
 import jakarta.ejb.Startup;
 import jakarta.ejb.Singleton;
-import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -32,46 +22,47 @@ import java.util.logging.Logger;
 @Singleton
 public class MqttService {
     private final String BROKER_IP_ADDRESS = "192.168.2.125";
-    private final String TELEMETRY_TOPIC = "v1/devices/me/telemetry/#";
+    private final String TELEMETRY_TOPIC = "v1/devices/me/telemetry";
     
-    private Mqtt5BlockingClient client;
+    private Mqtt5AsyncClient client;
+    private Mqtt5SubAck mqtt5SubAck = null;
     
     @PostConstruct
     public void init() {
         client = Mqtt5Client.builder()
                 .identifier(UUID.randomUUID().toString())
                 .serverHost(BROKER_IP_ADDRESS)
-                .buildBlocking();
+                .buildAsync();
         
-        Mqtt5ConnAck connect = client.connect();
-        System.out.println("Client connected with message: " + connect.toString());
+        client.connect()
+                .thenAccept(connAck -> System.out.println("Client connected with message: " + connAck));
         sottoscrivitiEAspetta();
     }
     
     public void sottoscrivitiEAspetta() {
-        Mqtt5SubAck mqtt5SubAck = null;
-        try (final Mqtt5Publishes publishes = client.publishes(MqttGlobalPublishFilter.ALL)) {
-
-            mqtt5SubAck = client.subscribeWith().topicFilter(TELEMETRY_TOPIC).qos(MqttQos.AT_LEAST_ONCE).send();
-
-            System.out.println("Aspetto un minuto per vedere se arrivano messaggi");
-            Optional<Mqtt5Publish> messaggioOptional = publishes.receive(1, TimeUnit.MINUTES);
-            if (messaggioOptional.isPresent())
-                System.out.println("Il valore del payload ricevuto è: " + new String(messaggioOptional.get().getPayloadAsBytes()));
-
-        } catch (InterruptedException ex) {
-            System.err.println(ex.getLocalizedMessage());
-        } finally {
-            if (mqtt5SubAck != null) {
-                client.unsubscribeWith().topicFilter(TELEMETRY_TOPIC).send();
-                System.out.println("Mi sono tolto dalla sottoscrizione");
-            }
-        }
+        mqtt5SubAck = client.subscribeWith()
+                .topicFilter(TELEMETRY_TOPIC)
+                .noLocal(true)                                      // we do not want to receive our own message
+                .retainHandling(Mqtt5RetainHandling.DO_NOT_SEND)    // do not send retained messages
+                .retainAsPublished(true)                            // keep the retained flag as it was published
+                .callback(
+                        publish -> maina(new String(publish.getPayloadAsBytes()))
+                )
+                .send()
+                .join();
+    }
+    
+    private void maina(String payload) {
+        System.out.println("Message: " + payload);
     }
     
     @PreDestroy
     public void destroy() {
-        client.disconnect();
-        System.out.println("Client disconnected");
+        if (mqtt5SubAck != null) {
+            client.unsubscribeWith().topicFilter(TELEMETRY_TOPIC).send();
+            System.out.println("Mi sono tolto dalla sottoscrizione");
+        }
+        client.disconnect()
+                .thenAccept(v -> System.out.println("Client disconnected"));
     }
 }

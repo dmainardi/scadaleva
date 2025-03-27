1e65873b7d382aedd85cfb6763251613a350cf0f
Davide Mainardi <davide@mainardisoluzioni.com>
2025-03-27 09:58:26 +0100

    Iniziato a creare le funzioni per accedere al database di Access.

--- src/main/resources/operations/create.sh
+++ src/main/resources/operations/create.sh
@@ -13,8 +13,14 @@ readonly DB_READ_ONLY_NAME=axis_files_lev
 readonly DB_READ_ONLY_USER_NAME=comandu
 readonly DB_READ_ONLY_USER_PASSWORD_ALIAS_NAME=scadaleva-db-readyonly-user-alias
 
+readonly IP_ACCESS_READ_ONLY_ADDRESS=192.168.2.248
+readonly DB_ACCESS_READ_ONLY_USER_NAME=produzione
+readonly DB_ACCESS_READ_ONLY_FOLDER=$HOME/"${APP_NAME}"/fustellatrice
+
 mkdir $HOME/"${APP_NAME}"
 mkdir $HOME/"${APP_NAME}"/documents
+mkdir "${DB_ACCESS_READ_ONLY_FOLDER}"
+sudo mount -t cifs -o credentials="${IDE_WORKSPACE}"/"${APP_NAME}"/src/main/resources/operations/accessCredential,dir_mode=0755,file_mode=0755 //"${IP_ACCESS_READ_ONLY_ADDRESS}"/Archivio $HOME/"${APP_NAME}"/fustellatrice
 \
 mvn -f "${IDE_WORKSPACE}"/"${APP_NAME}"/pom.xml -DincludeScope=provided -DexcludeGroupIds=jakarta.activation,jakarta.annotation,jakarta.authentication,jakarta.authorization,jakarta.batch,jakarta.ejb,jakarta.el,jakarta.enterprise,jakarta.faces,jakarta.inject,jakarta.interceptor,jakarta.jms,jakarta.json,jakarta.json.bind,jakarta.mail,jakarta.persistence,jakarta.platform,jakarta.resource,jakarta.security.enterprise,jakarta.servlet,jakarta.transaction,jakarta.validation,jakarta.websocket,jakarta.ws.rs -DoutputDirectory=$HOME/"${APP_NAME}"/ dependency:copy-dependencies
 \
@@ -48,5 +54,27 @@ ServerName="${IP_READ_ONLY_ADDRESS}":\
 portNumber="${TCP_READ_ONLY_PORT}" \
 mysql_readOnly_${APP_NAME}_pool
 ./asadmin create-jdbc-resource --connectionpoolid mysql_readOnly_"${APP_NAME}"_pool jdbc/mysql_readOnly_"${APP_NAME}"
+
+./asadmin create-jdbc-connection-pool \
+--datasourceclassname net.ucanaccess.jdbc.UcanaccessDataSource \
+--restype javax.sql.DataSource \
+--property \
+User="${DB_ACCESS_READ_ONLY_USER_NAME}":\
+Password=superSecretPassowordWorldWide:\
+AccessPath="${DB_ACCESS_READ_ONLY_FOLDER}"/ArchivioStorico.Mdb:\
+LoginTimeout=0 \
+access_readOnly_${APP_NAME}_archivioStorico_pool
+./asadmin create-jdbc-resource --connectionpoolid access_readOnly_${APP_NAME}_archivioStorico_pool jdbc/access_readOnly_"${APP_NAME}"_archivioStorico
+
+./asadmin create-jdbc-connection-pool \
+--datasourceclassname net.ucanaccess.jdbc.UcanaccessDataSource \
+--restype javax.sql.DataSource \
+--property \
+User="${DB_ACCESS_READ_ONLY_USER_NAME}":\
+Password=superSecretPassowordWorldWide:\
+AccessPath="${DB_ACCESS_READ_ONLY_FOLDER}"/archiviolavoro.mdb:\
+LoginTimeout=0 \
+access_readOnly_${APP_NAME}_archivioLavoro_pool
+./asadmin create-jdbc-resource --connectionpoolid access_readOnly_${APP_NAME}_archivioLavoro_pool jdbc/access_readOnly_"${APP_NAME}"_archivioLavoro
 
 ./asadmin stop-domain
--- src/main/resources/operations/destroy.sh
+++ src/main/resources/operations/destroy.sh
@@ -6,8 +6,13 @@ readonly DB_READ_ONLY_USER_PASSWORD_ALIAS_NAME=scadaleva-db-readyonly-user-alias
 
 ./asadmin delete-jdbc-resource jdbc/mysql_"${APP_NAME}"
 ./asadmin delete-jdbc-resource jdbc/mysql_readOnly_"${APP_NAME}"
+./asadmin delete-jdbc-resource jdbc/access_readOnly_"${APP_NAME}"_archivioStorico
+./asadmin delete-jdbc-resource jdbc/access_readOnly_"${APP_NAME}"_archivioLavoro
 ./asadmin delete-jdbc-connection-pool mysql_"${APP_NAME}"_pool
 ./asadmin delete-jdbc-connection-pool mysql_readOnly_"${APP_NAME}"_pool
+./asadmin delete-jdbc-connection-pool access_readOnly_"${APP_NAME}"_archivioStorico_pool
+./asadmin delete-jdbc-connection-pool access_readOnly_"${APP_NAME}"_archivioLavoro_pool
 ./asadmin delete-password-alias "${DB_READ_ONLY_USER_PASSWORD_ALIAS_NAME}"
 ls -p "${AS_LIBFOLDER}"/ | grep -v / | xargs -n1 ./asadmin remove-library
+sudo umount $HOME/"${APP_NAME}"/fustellatrice
 rm -r $HOME/"${APP_NAME}"/
